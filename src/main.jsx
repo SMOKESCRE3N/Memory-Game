7b4ce16b042c85fdb2959d4eda985aa251212b7b
SMOKESCRE3N <dixitkartik1203@gmail.com>
2025-03-30 18:38:24 +0530

    for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import {  StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import {BrowserRouter, RouterProvider,createBrowserRouter,createHashRouter} from "react-router-dom";
-import{BrowserRouter} from 'react-router-dom';
+
 import Game from './components/Game/Game.jsx';
 import Grid from './components/Grid/Grid.jsx'
 import Home from './components/Home/Home.jsx';
@@ -19,8 +19,8 @@ const router=createBrowserRouter([
 ]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <BrowserRouter basename="/Memory_Game/"> 
+ 
     <RouterProvider router={router} />
-  </BrowserRouter>
+  
   </StrictMode>,
 )
