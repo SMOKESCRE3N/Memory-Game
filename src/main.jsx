80968687ec0ab2ff16ee30a2d8a22c69476f704a
SMOKESCRE3N <dixitkartik1203@gmail.com>
2025-03-30 18:34:28 +0530

    update for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import {  StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {RouterProvider,createBrowserRouter,createHashRouter} from "react-router-dom";
-import Game from './components/Game/Game.jsx'
+import {BrowserRouter, RouterProvider,createBrowserRouter,createHashRouter} from "react-router-dom";
+import{BrowserRouter} from 'react-router-dom';
+import Game from './components/Game/Game.jsx';
 import Grid from './components/Grid/Grid.jsx'
 import Home from './components/Home/Home.jsx';
 
