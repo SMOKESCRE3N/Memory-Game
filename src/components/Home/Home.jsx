bc24920fff9be428bf577a37a1889f15d0b1bb24
SMOKESCRE3N <dixitkartik1203@gmail.com>
2025-03-30 22:46:56 +0530

    how to play

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -21,8 +21,8 @@ function App() {
       <header className="text-center mb-8">
         <h1 className="text-4xl font-bold text-white">Test your Memory!!</h1>
       </header>
-      <main>
-        <div>
+      <main >
+        <div className='flex items-center justify-center'>
             <NavLink
          
          to="/play"
@@ -34,6 +34,19 @@ function App() {
         
         </NavLink>
         </div>
+
+        <div className='  flex items-center justify-center text-xl pb-2 p-8 font-bold text-white'> 
+          HOW TO PLAY
+        </div>
+          <div className='flex items-center justify-center'>
+          <ul 
+          className=' list-disc text-xl font-semibold text-white'>
+            <li>Click each card only once to score.</li>
+            <li>Clicking the same card twice resets your score.</li>
+            <li>Cards shuffle after every click.</li>
+          </ul>
+          </div>
+        
       </main>
     </div>
   );
